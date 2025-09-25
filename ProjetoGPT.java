d32437ddae822d8247178875ccd26d48ab25f61a
TaylanCosta <taylancosta37@gmail.com>
2025-09-24 22:22:23 -0300

    errooo

--- ProjetoGPT.java
+++ ProjetoGPT.java
@@ -1,19 +1,1 @@
-import java.util.Scanner;
 
-public class ProjetoGPT {
-    public static void main(String[] args) {
-        // Solicitando o nome do usuário
-        try ( // Criando o objeto Scanner
-                Scanner input = new Scanner(System.in)) {
-            // Solicitando o nome do usuário
-            System.out.print("Digite seu nome: ");
-            String nome = input.nextLine();
-            // Solicitando a idade do usuário
-            System.out.print("Digite sua idade: ");
-            int idade = input.nextInt();
-            // Exibindo a mensagem
-            System.out.println("Olá, " + nome + "! Você tem " + idade + " anos.");
-            // Encerrando o Scanner
-        }
-    }
-}
